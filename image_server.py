d906b51533b078314351c5c9e2f87e15747d6609
kiahmonpara <kiahmonpara@gmail.com>
2025-04-21 18:54:42 +0530

    deployed

--- image_server.py
+++ image_server.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://sim-ppl-internship.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
